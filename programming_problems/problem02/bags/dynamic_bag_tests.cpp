e06dd5a037c04ce89a2c693021670f0b6a123828
Dominique Pantin <dlp80@pitt.edu>
2023-02-05 21:38:51 -0600

    almost done

--- programming_problems/problem02/bags/dynamic_bag_tests.cpp
+++ programming_problems/problem02/bags/dynamic_bag_tests.cpp
@@ -9,11 +9,90 @@ template class DynamicBag<int>;
 TEST_CASE("Calling all public members", "[DynamicBag]"){
   DynamicBag<int> b;
 
-  b.add(0);
-  b.remove(0);
-  b.isEmpty();
-  b.getCurrentSize();
-  b.clear();
+  b.add(0); //checked
+  b.remove(0); //checked
+  b.isEmpty(); //checked
+  b.getCurrentSize(); //checked
+  b.clear(); //checked
   b.getFrequencyOf(0);
   b.contains(0);
+}
+
+//tests if current size is correctly returned
+TEST_CASE("first case", "[Dynamic Bag]"){
+  DynamicBag<int> b;
+  b.add(13);
+  b.add(12);
+  b.add(10000);
+  b.add(4);
+  b.add(100);
+  b.add(33);
+
+  REQUIRE(b.getCurrentSize() == 6);
+}
+
+TEST_CASE("second case", "[Dynamic Bag]"){
+DynamicBag<int> b;
+
+  b.add(1);
+  b.add(438);
+  b.add(0);
+  b.add(12121);
+
+  b.remove(438);
+
+  REQUIRE_FALSE(b.contains(438));
+  REQUIRE_FALSE(b.isEmpty());
+}
+
+
+TEST_CASE("third case", "[Dynamic Bag]"){
+DynamicBag<int> b;
+
+  b.add(13);
+  b.add(12);
+
+  REQUIRE_FALSE(b.isEmpty());
+}
+
+TEST_CASE("fourth case", "[Dynamic Bag]"){
+DynamicBag<int> b;
+
+  b.add(13);
+  b.add(12);
+
+  REQUIRE_FALSE(b.contains(12));
+}
+
+TEST_CASE("fifth case", "[Dynamic Bag]"){
+DynamicBag<int> b;
+
+  b.add(1);
+  b.add(438);
+  b.add(0);
+  b.add(12121);
+
+  b.clear();
+
+  REQUIRE(b.isEmpty());
+}
+
+TEST_CASE("sixth case", "[Dynamic Bag]"){
+DynamicBag<int> b;
+
+  b.add(1);
+  b.add(438);
+  b.add(0);
+  b.add(12121);
+  b.add(10);
+  b.add(12121);
+  b.add(3);
+  b.add(12121);
+  b.add(55);
+  
+
+  b.remove(12121);
+  b.getFrequencyOf(12121);
+
+  REQUIRE(b.getFrequencyOf(12121) == 3);
 }
