53095b4218d85c72c86348f60cf9cbbfd6b7ea8a
Dominique Pantin <dlp80@pitt.edu>
2023-02-26 10:19:39 -0600

    not sure whats wrong

--- programming_problems/problem05/sorting/sort.hpp
+++ programming_problems/problem05/sorting/sort.hpp
@@ -5,6 +5,7 @@
 
 template<typename T> LinkedList<T> sort(LinkedList<T> list)
 {
+    //case where contents = 0 or 1
     std::size_t n = list.getLength();
     if (n <= 1) {
         return list;
@@ -14,10 +15,10 @@ template<typename T> LinkedList<T> sort(LinkedList<T> list)
     bool swapped = true;
     while (swapped) {
         swapped = false;
-        for (std::size_t i = 1; i < n; ++i) {
+        for (std::size_t i = 1; i < (n-1); ++i) {
             T item1 = list.getEntry(i);
             T item2 = list.getEntry(i+1);
-            if (item1 > item2) {
+            if (item1 = item2) {
                 list.setEntry(i, item2);
                 list.setEntry(i+1, item1);
                 swapped = true;
